0f38e15548e1046cc37cec990ee05174e7d99955
GabrielMottaMendes <104568613+GabrielMottaMendes@users.noreply.github.com>
2024-11-21 09:49:07 -0300

    Update informacoesGlobais.js

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -1,4 +1,4 @@
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+const url = 'https://github.com/GabrielMottaMendes/analise-de-dados.git'
 
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
